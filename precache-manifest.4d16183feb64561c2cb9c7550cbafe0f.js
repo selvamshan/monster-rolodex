0a352709cb60969ff06b866d8d4789625ffd3dd5
Selvaraj <selvaraj_s4@hotmail.com>
2019-10-02 17:12:37 +0530

    Updates

---
+++ precache-manifest.4d16183feb64561c2cb9c7550cbafe0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c764def5a2d30f6835b9790ceea81911",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "27d3094b4dde95ae8b74",
+    "url": "/monster-rolodex/static/css/main.c7a62ca3.chunk.css"
+  },
+  {
+    "revision": "cc7c9d650420ac088aa4",
+    "url": "/monster-rolodex/static/js/2.446a0095.chunk.js"
+  },
+  {
+    "revision": "27d3094b4dde95ae8b74",
+    "url": "/monster-rolodex/static/js/main.da3c31a8.chunk.js"
+  },
+  {
+    "revision": "f5a0a3606ee7f35ffc7f",
+    "url": "/monster-rolodex/static/js/runtime-main.ec8e1249.js"
+  }
+]);
